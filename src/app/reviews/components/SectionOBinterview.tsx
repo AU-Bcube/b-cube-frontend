0a604a8fcebd2ab8c21ca406927f68602592ad7d
suhwan kim <playground99@naver.com>
2025-02-04 00:54:46 +0900

    fix: css수정

--- src/app/reviews/components/SectionOBinterview.tsx
+++ src/app/reviews/components/SectionOBinterview.tsx
@@ -22,7 +22,7 @@ const SectionOBinterview: React.FC<SectionOBinterviewProps> = ({ postData }) => 
   };
 
   return (
-    <div className="flex flex-col justify-center items-center mt-[64px] max-w-7xl mx-auto md:px-12 px-8">
+    <div className="flex flex-col justify-center items-center mt-[64px] max-w-7xl mx-auto md:px-12 px-8 gap-10 md:gap-20">
       {/* 그리드 컨테이너 */}
       <div className="grid md:grid-cols-3 grid-cols-1 gap-6">
         {postData.slice(0, visibleCount).map((post, index, arr) => {
@@ -44,7 +44,7 @@ const SectionOBinterview: React.FC<SectionOBinterviewProps> = ({ postData }) => 
       </div>
       {/* "더보기" 버튼을 프로필 카드 아래에 배치 */}
       {visibleCount < postData.length && (
-        <section className="flex justify-center items-center w-full mt-24">
+        <section className="flex justify-center items-center w-full md:mb-20 mb-10">
           <ActivityButton
             activity="더보기"
             selected={true}
