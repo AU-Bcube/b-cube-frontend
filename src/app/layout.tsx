81b4352f66de9b2e2506a0d5ef37cf67956389ef
suhwan kim <playground99@naver.com>
2025-02-04 13:17:14 +0900

    add:메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const pretendard = localFont({ // ⬅️ add
 });
 
 export const metadata: Metadata = {
-  title: "B-cube 비큐브",
+  title: "비큐브 B-cube",
   description: "아주대학교 경영인텔리전스학과 소학회",
   icons: {
     icon: "/logo.svg"
@@ -22,11 +22,11 @@ export const metadata: Metadata = {
   keywords: ["B-cube", "비큐브", "아주대학교", "경영인텔리전스학과", "소학회", "이비즈", "e-비즈니스학과", "e-business", "IT기획", "웹개발", "앱개발", "백엔드", "프론트엔드", "피그마"],
   metadataBase: new URL("https://b-cube.kr"),
   openGraph: {
-    title: "B-cube",
+    title: "비큐브 B-cube",
     description: "아주대학교 경영인텔리전스학과 소학회",
     images: "/opengraph-image.png",
     url: "https://b-cube.kr",
-    siteName: "B-cube",
+    siteName: "비큐브 B-cube",
     locale: "ko_KR",
     type: "website",
   },
