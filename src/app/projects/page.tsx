58e6128867bb699074fd1321c8fce677e118d426
suhwan kim <playground99@naver.com>
2025-02-17 21:44:34 +0900

    fix: 데이터 페칭시 캐싱 안하게 수정

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,7 +7,7 @@ import ProjectSections from "./components/ProjectSections";
 export default async function Main() {
   
   const designthon = await getDesignton();
-  const sexyIt = await getSexyIt();
+  const sexyIt = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/sexyit`, {cache:"no-store"}).then((res) => res.json());
   const study = await getStudy();
   const etc = await getEtc();
 
