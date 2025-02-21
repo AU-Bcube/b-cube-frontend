5dfb2f9c120004f9cc47c1a885c823ba74e05658
suhwan kim <playground99@naver.com>
2025-02-21 17:45:27 +0900

    회장단 순서 변경

--- src/app/(home)/components/Executives.tsx
+++ src/app/(home)/components/Executives.tsx
@@ -23,7 +23,7 @@ export default async function Executives() {
   console.log(thisYear);
   const currentExecutive = executives
   .filter((executive: Executive) => executive.year == thisYear)
-  .sort((a: Executive, b: Executive) => (a.role === "회장 " ? -1 : b.role === "회장 " ? 1: 0))
+  .sort((a: Executive, b: Executive) => (a.role === "회장 " ? -1 : b.role === "회장 " ? 1: 0)) //회장이 먼저오게 정렬
 
 
   if (currentExecutive.length === 0) {
