2b18cd823f2cda726b26ec82ecf8346269de93b3
suhwan kim <playground99@naver.com>
2025-02-23 19:51:36 +0900

    add: api요청에 referer헤더 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,9 @@
 export const defaultFetch = async(location: string) => {
-  return await fetch(`${process.env.NEXT_PUBLIC_API_HOST}${location}`, {cache:"no-store"}).then((res) => res.json());
+  return await fetch(`${process.env.NEXT_PUBLIC_API_HOST}${location}`, {
+    cache:"no-store", 
+    headers: {
+      Referer: 'https://b-cube.kr',
+      'Content-Type': 'application/json',
+    },
+  },).then((res) => res.json());
 }
