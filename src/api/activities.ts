0a18150bf73bf5013c323aaced4346cf94bc2da8
suhwan kim <playground99@naver.com>
2025-11-27 14:53:42 +0900

    redeploy

--- src/api/activities.ts
+++ src/api/activities.ts
@@ -1,14 +1,13 @@
-import { defaultFetch } from "./api"
+import { defaultFetch } from './api';
 
 const getActivities = async () => {
   try {
-    const response = await defaultFetch("/activities");
+    const response = await defaultFetch('/activities');
     return response;
   } catch (error) {
-    console.error("활동 데이터를 가져오는 중 오류 발생:", error);
+    console.error('활동 데이터를 가져오는 중 오류 발생: ', error);
     return [];
   }
 };
-
 
 export { getActivities };
