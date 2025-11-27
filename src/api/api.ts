46b31fdc389ea820f47a592df0bc3bccd590c390
suhwan kim <playground99@naver.com>
2025-11-27 14:28:27 +0900

    fix:깃 시크릿 수정

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,6 @@ export const defaultFetch = async (location: string) => {
   return await fetch(`${process.env.NEXT_PUBLIC_API_HOST}${location}`, {
     cache: 'no-store',
     headers: {
-      Referer: 'http://b-cube.kr',
       'Content-Type': 'application/json',
     },
   }).then((res) => res.json());
